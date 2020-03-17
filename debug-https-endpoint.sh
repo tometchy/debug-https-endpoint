3f53b263699dab649a0b67bfe818dcb484f18509
tometchy <tometchy@gmail.com>
2020-03-17 17:58:18 +0100

    Implement endpoint investigation

---
+++ debug-https-endpoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z "$TARGET" ]
+then
+      echo "TARGET environment variable is not assigned, exiting";
+      exit 1;
+fi
+
+if [ -n "$SLEEP" ]
+then
+      echo "SLEEP environment variable assigned, waiting ${SLEEP} seconds";
+      sleep $SLEEP;
+fi
+
+echo "Investigating target endpoint: $TARGET";
+
+if [ "$DUMP_TRAFFIC" == "y" ]; then
+      echo "DUMP_TRAFFIC environment variable set to: 'y', printing extensive debugging information, including a hex dump of all traffic";
+      openssl s_client -connect $TARGET -debug -state;
+else
+      openssl s_client -connect $TARGET -state;
+fi
